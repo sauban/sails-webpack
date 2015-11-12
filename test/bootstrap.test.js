c91233c98e6d2e80a6bdd71e3440742a77e268ab
Travis Webb <me@traviswebb.com>
2015-11-12 14:17:49 -0600

    add simple bootstrap test

---
+++ test/bootstrap.test.js
@@ -0,0 +1,24 @@
+var _ = require('lodash');
+var Sails = require('sails');
+var path = require('path');
+var sails;
+
+before(function(done) {
+  this.timeout(30000);
+
+  var config = {
+    appPath: path.resolve(__dirname, '..')
+  }
+
+  Sails.lift(config, function(err, server) {
+    if (err) return done(err);
+
+    global.sails = server;
+    done(err, sails);
+  });
+
+});
+
+after(function(done) {
+  global.sails.lower(done);
+});
